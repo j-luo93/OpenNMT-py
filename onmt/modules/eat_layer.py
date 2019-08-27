6a8f57ee8ea965c6f9f2c012d0cdac35338e0d38
j-luo93 <j_luo@mit.edu>
2019-08-27 02:13:19 -0400

    residual eat layer

--- onmt/modules/eat_layer.py
+++ onmt/modules/eat_layer.py
@@ -2,17 +2,22 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from onmt.utils import aeq
+
 
 class EatLayer(nn.Module):
 
-    def __init__(self, d_in, d_hid, d_out, mode='nonlinear'):
+    def __init__(self, d_in, d_hid, d_out, mode='residual'):
         super().__init__()
         self.event_hidden_layer = nn.Linear(d_in, d_hid)
         self.agent_hidden_layer = nn.Linear(d_in, d_hid)
         self.theme_hidden_layer = nn.Linear(d_in, d_hid)
         self.hidden = nn.Linear(d_hid * 3, d_out)
-        assert mode in ['nonlinear', 'sum']
+
+        assert mode in ['nonlinear', 'sum', 'residual']
         self.mode = mode
+        if self.mode == 'residual':
+            aeq(d_in, d_out)
 
     def forward(self, event, agent, theme):
         if self.mode == 'sum':
@@ -23,4 +28,6 @@ class EatLayer(nn.Module):
         theme_hidden = self.theme_hidden_layer(theme)
         cat = torch.cat([event_hidden, agent_hidden, theme_hidden], dim=-1)
         output = self.hidden(F.leaky_relu(cat, negative_slope=0.1))
+        if self.mode == 'residual':
+            output = output + event + agent + theme
         return output
--- onmt/tests/test_eat.py
+++ onmt/tests/test_eat.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 from onmt.encoders.transformer import TransformerEatEncoder
 from onmt.modules.eat_layer import EatLayer
+from onmt.modules.embeddings import Embeddings
 
 DIM = 25
 SEQ_LEN = 15
@@ -27,10 +28,11 @@ class TestEatLayer(TestCase):
 class TestTransformerEat(TestCase):
 
     def setUp(self):
-        emb = nn.Embedding(VOCAB_SIZE, DIM)
+        emb = Embeddings(DIM, VOCAB_SIZE, 0)
         self.encoder = TransformerEatEncoder(1, DIM, 5, DIM, 0.1, 0.1, emb, 0)
 
     def test_basic(self):
-        src = torch.randint(VOCAB_SIZE, (SEQ_LEN * 3, BATCH_SIZE))
-        lengths = torch.randint(SEQ_LEN, (BATCH_SIZE,)) + 1
+        src = torch.randint(VOCAB_SIZE, (SEQ_LEN * 3, BATCH_SIZE, 1))
+        lengths = (torch.randint(SEQ_LEN, (BATCH_SIZE,)) + 1) * 3
+        lengths[0] = SEQ_LEN * 3
         emb, out, lengths = self.encoder(src, lengths=lengths)
