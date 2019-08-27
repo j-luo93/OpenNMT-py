e17308d000ba866137cb06db1091a93ecace9238
j-luo93 <j_luo@mit.edu>
2019-08-27 00:03:51 -0400

    better script and eat layer

--- onmt/modules/eat_layer.py
+++ onmt/modules/eat_layer.py
@@ -1,20 +1,26 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class EatLayer(nn.Module):
 
-    def __init__(self, d_in, d_hid, d_out):
+    def __init__(self, d_in, d_hid, d_out, mode='nonlinear'):
         super().__init__()
         self.event_hidden_layer = nn.Linear(d_in, d_hid)
         self.agent_hidden_layer = nn.Linear(d_in, d_hid)
         self.theme_hidden_layer = nn.Linear(d_in, d_hid)
         self.hidden = nn.Linear(d_hid * 3, d_out)
+        assert mode in ['nonlinear', 'sum']
+        self.mode = mode
 
     def forward(self, event, agent, theme):
+        if self.mode == 'sum':
+            return event + agent + theme
+
         event_hidden = self.event_hidden_layer(event)
         agent_hidden = self.agent_hidden_layer(agent)
         theme_hidden = self.theme_hidden_layer(theme)
         cat = torch.cat([event_hidden, agent_hidden, theme_hidden], dim=-1)
-        output = self.hidden(cat)
+        output = self.hidden(F.leaky_relu(cat, negative_slope=0.1))
         return output
--- transformer.sh
+++ transformer.sh
@@ -1,1 +1,6 @@
-python -m ipdb -c continue train.py -data data/data -save_model /tmp/extra -layers 1 -rnn_size 512 -word_vec_size 512 -transformer_ff 512 -heads 8 -encoder_type eat -decoder_type eat -position_encoding -train_steps 20000 -max_generator_batches 2 -dropout 0.1 -batch_size 128 -batch_type tokens -normalization tokens -accum_count 2 -optim adam -adam_beta2 0.998 -decay_method noam -warmup_steps 1000 -learning_rate 2 -max_grad_norm 0 -param_init 0 -param_init_glorot -label_smoothing 0.1 -valid_steps 1000 -save_checkpoint_steps 1000 
+gpu=$1
+data=$2
+save=$3
+#type=transformer
+type=eat
+CUDA_VISIBLE_DEVICES=$gpu python -m ipdb -c continue train.py -data data/$data -save_model ~/tmp/$save -layers 6 -rnn_size 512 -word_vec_size 512 -transformer_ff 2096 -heads 8 -encoder_type $type -decoder_type $type -position_encoding -train_steps 20000 -max_generator_batches 2 -dropout 0.1 -batch_size 128 -batch_type sents -normalization sents -accum_count 2 -optim adam -adam_beta2 0.998 -decay_method noam -warmup_steps 8000 -learning_rate 2 -max_grad_norm 0 -param_init 0 -param_init_glorot -label_smoothing 0.1 -valid_steps 1000 -save_checkpoint_steps 1000 -world_size 1  -gpu_ranks 0
